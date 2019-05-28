bc950631aee9d6205cf2e60bdab321d4629140c1
wanaka <asd8116@gmail.com>
2019-05-28 16:00:34 +0800

    Add html and javascript

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,1 +1,29 @@
-const audioPanel = document.getElementById('audio-panel')
+function removeTransition(e) {
+  if (e.propertyName === 'transform') {
+    e.target.classList.remove('playing')
+  }
+}
+
+function playSound(e) {
+  let keyOrNot = e.keyCode || this.getAttribute('data-key') // 判斷觸發點
+
+  const audio = document.querySelector(`audio[data-key="${keyOrNot}"]`)
+  const key = document.querySelector(`.key[data-key="${keyOrNot}"]`)
+
+  if (audio) {
+    audio.currentTime = 0
+    audio.play()
+  }
+
+  if (key) {
+    key.classList.add('playing')
+  }
+}
+
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)) // 結束時移除特效
+
+keys.forEach(key => key.addEventListener('click', playSound)) // 畫面點擊
+
+window.addEventListener('keydown', playSound) // 鍵盤點擊
